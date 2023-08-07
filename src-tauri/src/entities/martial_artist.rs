f278238024f4391f0d426ad018fe194ea85924f4
hmidassi <hedy.midassi@gmail.com>
2023-08-07 23:43:15 +0200

    Initializing some structures for data model

---
+++ src-tauri/src/entities/martial_artist.rs
@@ -0,0 +1,10 @@
+use experience_module::Experience;
+use std::collections::Vec;
+
+mod martial_artist_mod{
+    pub struct MartialArtist{
+        first_name: String,
+        last_name: String,
+        experiences: Vec<Experience>
+    }
+}
