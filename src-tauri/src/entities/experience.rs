78ed87f7ccb3855e0ef3eae8217cabcf9164f37b
hmidassi <hedy.midassi@gmail.com>
2023-08-08 22:36:39 +0200

    Updating experience: vec + club_name

--- src-tauri/src/entities/experience.rs
+++ src-tauri/src/entities/experience.rs
@@ -3,6 +3,7 @@ use martial_arts::MartialArt;
 
 mod experience_module {
     pub struct Experience {
+        club_name: String,
         start_date: NaiveDate,
         end_date: NaiveDate,
         martial_arts: Vec<MartialArt>,
