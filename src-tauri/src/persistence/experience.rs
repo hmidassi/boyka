56f34d9a35b4f016bcdda7492a7d8c3f66aece62
hmidassi <hedy.midassi@gmail.com>
2023-08-11 13:46:31 +0200

    Experimenting with enums: adding past and current XP (To be confirmed with diesel)

--- src-tauri/src/persistence/experience.rs
+++ src-tauri/src/persistence/experience.rs
@@ -2,11 +2,15 @@ use chrono::NaiveDate;
 use martial_arts::MartialArt;
 
 mod experience_module {
-    pub struct Experience {
+    enum Experience{
+        CurrentExperience{experience: GeneralExperience},
+        PastExperience{experience: GeneralExperience, end_date: NaiveDate}
+    }
+
+    pub struct GeneralExperience {
         id: i16,
         club_name: String,
         starting_date: NaiveDate,
-        end_date: NaiveDate,
         //one club may offer several martial arts at once
         martial_arts: Vec<MartialArt>,
         //find a smarter way to associate each martial art and its average duration per
